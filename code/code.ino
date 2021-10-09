1bed89cba4113c6fb72a38d94caccfcdc3623a59
Krzysztof Jankowski <krzysztof.jankowski@beffio.com>
2021-10-09 19:15:34 +0200

    updatedjs code

--- code/code.ino
+++ code/code.ino
@@ -1,26 +1,48 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h> 
 #include <FS.h>
+#include <DHT.h>
 
 const char* ssid = "P1X_2.4GHz"; //Enter SSID
 const char* password = "dawajneta"; //Enter Password
 
-ESP8266WebServer server(80); 
+#define DHTPIN 5     
+#define DHTTYPE    DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+float sens_temp = 0.0;
+float sens_humi = 0.0;
+float sens_mois = 0.0;
+
+const char* html_temp = "%TEMP%";
+const char* html_humi = "%HUMI%";
+const char* html_mois = "%MOIS%";
+const char* html_status_color = "%SCOLOR%";
+const char* html_status_icon = "%SICON%";
+const char* html_status_msg = "%SMSG%";
+const char* html_status_time = "%TIMESTAMP%";
+
+const char* icon_ok = "👍";
+const char* icon_water = "🌊";
+const char* icon_hot = "🔥";
+const char* icon_cold = "☃";
+
+ESP8266WebServer server(80);
 
 String getContentType(String filename);
 bool handleFileRead(String path);
+void updateSensorsReadings();
 
 void setup(void){
     Serial.begin(115200);
-    delay(1000);
     Serial.println('\n');
     delay(1000);
     Serial.println("----------------------------------------");
-    Serial.println("WELCOME TO THE < PLANT BEST FRIEND >");
+    Serial.println("  WELCOME TO THE < PLANT BEST FRIEND >  ");
     Serial.println("----------------------------------------");
     delay(1000);
     
-    Serial.println("> Connecting to WiFi [");
+    Serial.print("> Connecting to WiFi [");
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) 
     {
@@ -31,8 +53,7 @@ void setup(void){
     Serial.println("> WiFi connection Successful");
 
     Serial.println("----------------------------------------");    
-    Serial.println("> The IP Address of ESP8266 Module is: ");
-    Serial.print("> ");
+    Serial.print("> The IP Address of ESP8266 Module is: ");
     Serial.println(WiFi.localIP());
     Serial.println("----------------------------------------");
 
@@ -42,7 +63,27 @@ void setup(void){
         server.send(404, "text/plain", "404: Not Found");
     });
     server.begin();
+    
+    server.on("/temp", []() {
+        server.send(200, "text/plain", String(sens_temp, DEC));
+    });
+    server.on("/humi", []() {
+        server.send(200, "text/plain", String(sens_humi, DEC));
+    });
+    server.on("/mois", []() {
+        server.send(200, "text/plain", String(sens_mois, DEC));
+    });
+    server.on("/update", []() {
+      updateSensorsReadings();
+      server.send(200, "text/plain", "OK");
+    });
+    
     Serial.println("> HTTP server started");
+
+    Serial.println("----------------------------------------");
+    Serial.println("> Updating sensor readings...");
+    updateSensorsReadings();
+    Serial.println("----------------------------------------");
 }
 
 void loop(void){
@@ -58,15 +99,31 @@ String getContentType(String filename) {
 }
 
 bool handleFileRead(String path) {
-  Serial.println("> HandleFileRead: " + path);
-  if (path.endsWith("/")) path += "index.html";
+  Serial.println("\t-> Serving file: " + path);
+  if (path.endsWith("/")) {
+    path += "index.html";
+    updateSensorsReadings();
+  }
   String contentType = getContentType(path);
+  String indexData;
+   
   if (SPIFFS.exists(path)) {
     File file = SPIFFS.open(path, "r");
     size_t sent = server.streamFile(file, contentType);
     file.close();
+    
     return true;
   }
-  Serial.println("\tFile Not Found");
-  return false;                                         // If the file doesn't exist, return false
+  Serial.println("\t-> File Not Found");
+  return false;
+}
+
+void updateSensorsReadings() {
+  sens_temp = dht.readTemperature();
+  sens_humi = dht.readHumidity();
+  sens_mois = 0.0;
+  Serial.println("> Sensors reading: ");
+  Serial.println("\t-> Temperature: " + String(sens_temp, DEC) + "°C");
+  Serial.println("\t-> Humidity: " + String(sens_humi, DEC) + "%");
+  Serial.println("\t-> Moisure: " + String(sens_mois, DEC) + "%");
 }
